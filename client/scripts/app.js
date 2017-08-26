0416e5430dd1677a15f7637e972531bf5d845902
Julianne Johnson <tweetypie417@yahoo.com>
2017-08-26 14:35:14 -0500

    last commit before solution video

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,9 @@
-var Message = function() {
-
-};
+var Message = function() {};
 
 var app = {};
 
 app.send = function (message) {
   $.ajax({
-    // This is the url you should use to communicate with the parse API server.
     url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
@@ -15,16 +12,40 @@ app.send = function (message) {
       console.log('chatterbox: Message sent', data, message);
     },
     error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
 };
 
-
-app.init = function() {
-
+app.fetch = function(message) {
+  $.ajax({
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Messages retrieved', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to retreive messages', data);
+    }
+  });
 };
+
+
+app.init = function() {};
+
+app.clearMessages = function() {};
+
+app.renderMessage = function(message) {
+
+  var $message = $("<div>", {class: "message"});
+   
+  $('#chats').prepend($message);
+  
+};
+
+app.renderRoom = function() {};
 
 $('.submitbutton').on('click', function() {
   var newMessage = new Message();
@@ -34,4 +55,6 @@ $('.submitbutton').on('click', function() {
   newMessage.roomname = $('[selected="selected"]').val();
 
   app.send(newMessage);
+  //app.fetch();
+  app.renderMessage(newMessage);
 });
